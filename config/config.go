9a66ebf502780cd5836c74183684ab13935691db
Jason <none>
2023-09-12 16:12:25 +0800

    add eip-1559 default gas

--- config/config.go
+++ config/config.go
@@ -2,9 +2,10 @@ package config
 
 // 默认gas limit估算失败后，21000 * 3 = 63000
 const (
-	DefaultContractGasLimit = "63000"
-	DefaultEthGasLimit      = "21000"
-	DefaultEthGasPrice      = "12000000000000" // 当前网络 standard gas price
+	DefaultContractGasLimit     = "63000"
+	DefaultEthGasLimit          = "21000"
+	DefaultEthGasPrice          = "12000000000000" // 当前网络 standard gas price
+	DefaultMaxPriorityFeePerGas = "1200000000000"  // 当前网络 standard gas price
 
 	GasFactor = 1.8
 )
