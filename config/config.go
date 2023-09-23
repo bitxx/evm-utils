bf1ede0a9e8dbda532c885933aca7bd048cf08fc
Jason <>
2023-09-23 22:51:15 +0800

    change the gas price

--- config/config.go
+++ config/config.go
@@ -4,8 +4,8 @@ package config
 const (
 	DefaultContractGasLimit     = "63000"
 	DefaultEthGasLimit          = "21000"
-	DefaultEthGasPrice          = "1300000000000" // 当前网络 standard gas price
-	DefaultMaxPriorityFeePerGas = "125000000000"  // 当前网络 standard gas price
+	DefaultEthGasPrice          = "50000000000"  // 当前网络 standard gas price
+	DefaultMaxPriorityFeePerGas = "125000000000" // 当前网络 standard gas price eip1159
 
 	GasFactor = 1.8
 )
